5020fea616df8ca859bfa562161eb667a484fc07
Tom Piscitell <tom@trusona.com>
2021-02-08 11:04:33 -0500

    initial commit

--- bin/test-cdk-pipeline.ts
+++ bin/test-cdk-pipeline.ts
@@ -1,7 +1,51 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { TestCdkPipelineStack } from '../lib/test-cdk-pipeline-stack';
+import { GitHubSourceAction } from '@aws-cdk/aws-codepipeline-actions';
+import { CdkPipeline, SimpleSynthAction } from '@aws-cdk/pipelines';
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import { SecretValue } from '@aws-cdk/core';
+
+
+class TestStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+  }
+}
+class TestStage extends cdk.Stage {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StageProps) {
+    super(scope, id, props);
+
+    new TestStack(this, 'Test');
+  }
+}
+
+class TestCdkPipelineStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const sourceArtifact = new codepipeline.Artifact();
+    const cloudAssemblyArtifact = new codepipeline.Artifact();
+
+    const pipeline = new CdkPipeline(this, 'Pipeline', {
+      cloudAssemblyArtifact,
+      sourceAction: new GitHubSourceAction({
+        actionName: 'Github',
+        output: sourceArtifact,
+        oauthToken: SecretValue.secretsManager('githubToken'),
+        owner: 'tompiscitell',
+        repo: 'test-cdk-pipeline-stack'
+      }),
+      synthAction: SimpleSynthAction.standardNpmSynth({
+        sourceArtifact,
+        cloudAssemblyArtifact,
+      })
+    })
+
+    pipeline.addApplicationStage(new TestStage(this, 'Test', { env: { account: '111111111111', region: 'us-west-2' }}))
+  }
+}
+
 
 const app = new cdk.App();
-new TestCdkPipelineStack(app, 'TestCdkPipelineStack');
+new TestCdkPipelineStack(app, 'TestCdkPipelineStack', { env: { account: '222222222222', region: 'us-west-2'}});
